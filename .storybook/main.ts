e54935db29ef00baf42fe7589fd60411cf3945b7
sunduckcow <sunduckcow@yandex-team.ru>
2025-03-02 00:48:37 +0300

    added theme switch to stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,30 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    {
+      name: "@storybook/addon-essentials",
+      options: {
+        actions: true,
+        backgrounds: false,
+        controls: true,
+        docs: true,
+        viewport: false,
+        toolbars: true,
+        measure: false,
+        outline: false,
+        highlight: true,
+      },
+    },
+    "@storybook/addon-themes",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,16 +1,20 @@
-import type { Preview } from "@storybook/react";
+import type { Preview, ReactRenderer } from "@storybook/react";
+import { withThemeByClassName } from "@storybook/addon-themes";
 
 import "../src/index.css";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+  parameters: {},
+  initialGlobals: { backgrounds: { value: "dark" } },
+  decorators: [
+    withThemeByClassName<ReactRenderer>({
+      themes: {
+        light: "",
+        dark: "dark",
       },
-    },
-  },
+      defaultTheme: "light",
+    }),
+  ],
 };
 
 export default preview;
