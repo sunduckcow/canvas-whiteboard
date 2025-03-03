c671d8ce5b4787c889bab6c5c26ed900979af21d
sunduckcow <sunduckcow@yandex-team.ru>
2025-03-04 00:34:45 +0300

    initialized useGestures hook

---
+++ src/hooks/useGestures.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { useGestures } from "./useGestures";
+import { Button } from "@/components/ui/button";
+import { RefreshCcw } from "lucide-react";
+
+const meta = {
+  title: "useGestures",
+  parameters: { layout: "centered" },
+} satisfies Meta<typeof useGestures>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render() {
+    const { x, y, z, ref, reset } = useGestures();
+    return (
+      <div className="[&>:not(:first-child)]:mt-8">
+        <Button onClick={reset}>
+          <RefreshCcw />
+          Reset
+        </Button>
+        <div
+          className="border border-border w-[300px] h-[300px] relative overflow-hidden"
+          ref={ref}
+        >
+          {Array(5)
+            .fill(0)
+            .map((_1, i) =>
+              Array(5)
+                .fill(0)
+                .map((_2, j) =>
+                  (i + j + 1) % 2 ? (
+                    <div
+                      key={`${i},${j}`}
+                      className="w-8 h-8 bg-green-300 dark:bg-green-700 flex items-center justify-center absolute"
+                      style={{
+                        transform: `translate(${x + i * 32 * z}px, ${
+                          y + j * 32 * z
+                        }px) scale(${z}, ${z})`,
+                        transformOrigin: "top left",
+                      }}
+                    >
+                      {i},{j}
+                    </div>
+                  ) : null
+                )
+            )}
+        </div>
+      </div>
+    );
+  },
+  args: {},
+};
---
+++ src/hooks/useGestures.tsx
@@ -0,0 +1,78 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+
+type EventListenersMap = {
+  [key in keyof HTMLElementEventMap]: (ev: HTMLElementEventMap[key]) => void;
+};
+
+const getRealCoordinates = (e: MouseEvent, parent: HTMLElement) => {
+  const { clientX, clientY } = e;
+  const bounds = parent.getBoundingClientRect();
+  const x = Math.round(clientX - bounds.left);
+  const y = Math.round(clientY - bounds.top);
+  return [x, y] as const;
+};
+
+interface UseGesturesParams {
+  x?: number;
+  y?: number;
+  z?: number;
+  speed?: number;
+}
+
+export function useGestures({
+  x: _x = 0,
+  y: _y = 0,
+  z: _z = 1,
+  speed = 2,
+}: UseGesturesParams = {}) {
+  const [{ x, y, z }, setPosition] = useState({ x: _x, y: _y, z: _z });
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const parent = ref.current;
+    if (!parent) return;
+    const wheelListener: EventListenersMap["wheel"] = (e) => {
+      e.preventDefault();
+      if (e.ctrlKey) {
+        const [deltaX, deltaY] = getRealCoordinates(e, parent);
+
+        const zoom = Math.exp(-e.deltaY / (100 / speed));
+        setPosition(({ x, y, z }) => ({
+          x: x * zoom + deltaX * (1 - zoom),
+          y: y * zoom + deltaY * (1 - zoom),
+          z: z * zoom,
+        }));
+      } else {
+        setPosition(({ x, y, z }) => ({
+          x: x - e.deltaX * speed,
+          y: y - e.deltaY * speed,
+          z,
+        }));
+      }
+    };
+    parent.addEventListener("wheel", wheelListener);
+    return () => {
+      parent.removeEventListener("wheel", wheelListener);
+    };
+  }, [speed, setPosition]);
+
+  const reset = useCallback(() => {
+    setPosition({ x: _x, y: _y, z: _z });
+  }, [_x, _y, _z, setPosition]);
+
+  return { x, y, z, ref, reset, setPosition };
+}
+
+/**
+ * defaults,
+ * settings (when apply action, which button pressed etc),
+ * materials (ref),
+ * transformers (process resulting value)
+ * -> useGestures ->
+ * raw gestures: delta-move, delta-rotate, delta-scale
+ *
+ * x, y, z should be controlled from outside
+ *
+ * + refactor
+ *
+ * NOTE: rotate in safari example: https://kenneth.io/post/detecting-multi-touch-trackpad-gestures-in-javascript
+ */
